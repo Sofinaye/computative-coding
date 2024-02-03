2fb258b462eaf1d43ce941f3478a757203a4b882
Abenezer <93038957+Sofinaye@users.noreply.github.com>
2024-02-03 22:29:40 +0200

    Iterating through the list and to create the appropiate sub list

--- maxSumPartitioning.py
+++ maxSumPartitioning.py
@@ -3,4 +3,13 @@ def maxSumAfterPartitioning(self, arr, k):
       count = k
       sub = []
       temp = arr.pop()
+      for i in range(len(arr)+1):
+            if count > 0:
+                sub.append(temp)
+                count -= 1
+            else:
+                temp = arr.pop()
+                sub.append(temp)
+                count = k -1
+        return sum(sub)
       
