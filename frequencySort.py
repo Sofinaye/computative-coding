8e957770be7678fdd93f9d6ad72fc3b90bcbdb08
Abenezer <93038957+Sofinaye@users.noreply.github.com>
2024-02-08 09:24:16 +0200

    we do not need to sort it in its character sequence

--- frequencySort.py
+++ frequencySort.py
@@ -7,7 +7,7 @@ def frequencySort(self, s):
       collection = list(hashMap.keys())
       for j in collection:
           result.append({"key":j, "value":hashMap[j]})
-      result=sorted(result, key=lambda d: d["key"])
+      # result=sorted(result, key=lambda d: d["key"])
       result=sorted(result, key=lambda d: d["value"], reverse = True)    
       for k in result:
           final += k["key"] * k["value"]
